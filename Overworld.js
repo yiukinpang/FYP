3df2800030d0f8756cfb8667523d5ac5c0ad2994
yiukinpang <112676900+yiukinpang@users.noreply.github.com>
2023-09-19 23:30:14 +0000

    test movement

---
+++ Overworld.js
@@ -0,0 +1,43 @@
+class Overworld {
+ constructor(config) {
+   this.element = config.element;
+   this.canvas = this.element.querySelector(".game-canvas");
+   this.ctx = this.canvas.getContext("2d");
+   this.map = null;
+ }
+
+  startGameLoop() {
+    const step = () => {
+      //Clear off the canvas
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+      //Draw Lower layer
+      this.map.drawLowerImage(this.ctx);
+
+      //Draw Game Objects
+      Object.values(this.map.gameObjects).forEach(object => {
+        object.update({
+          arrow: this.directionInput.direction
+        })
+        object.sprite.draw(this.ctx);
+      })
+
+      //Draw Upper layer
+      this.map.drawUpperImage(this.ctx);
+      
+      requestAnimationFrame(() => {
+        step();   
+      })
+    }
+    step();
+ }
+
+ init() {
+  this.map = new OverworldMap(window.OverworldMaps.DemoRoom);
+
+  this.directionInput = new DirectionInput();
+  this.directionInput.init();
+
+  this.startGameLoop();
+ }
+}
