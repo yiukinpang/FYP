3d7b3571f5059cc7f9883ed1ba1a9222415e99d0
LeeKaChing <112677095+LeeKaChing@users.noreply.github.com>
2024-05-01 16:43:54 +0800

    update text file

--- TextMessage.js
+++ TextMessage.js
@@ -3,37 +3,72 @@ class TextMessage {
     this.text = text;
     this.onComplete = onComplete;
     this.element = null;
+    this.ResponseQuestion = null;
+    this.ResponseChoices = null;
+    this.ResponseAnswer = null;
+  }
+
+  getQuestion() {
+    fetch("https://us-central1-fyp-a08.cloudfunctions.net/get_question")
+      .then(response => response.json())
+      .then(data => {
+        this.ResponseQuestion = data.question;
+        this.ResponseChoices = data.choices;
+        this.ResponseAnswer = data.answer;
+        this.changeQuestionContext();
+      })
+      .catch(error => {
+        console.error("Error fetching question:", error);
+      });
+  }
+
+  changeQuestionContext() {
+    document.getElementById("TextMessage").innerText = this.ResponseQuestion;
+    document.getElementById("selectA").innerText = "A: "+ this.ResponseChoices.A;
+    document.getElementById("selectB").innerText = "B: "+ this.ResponseChoices.B;
+    document.getElementById("selectC").innerText = "C: "+ this.ResponseChoices.C;
+    document.getElementById("selectD").innerText = "D: "+ this.ResponseChoices.D;
+  }
+
+  verifyAnswer() {
   }
 
   createElement() {
-    //Create the element
+    // Create the element
     this.element = document.createElement("div");
     this.element.classList.add("TextMessage");
 
-    this.element.innerHTML = (`
-      <p class="TextMessage_p"></p>
-      <button class="TextMessage_button">Next</button>
-    `)
+    this.element.innerHTML = `
+      <p class="TextMessage_p" id="TextMessage"></p>
+      <button class="Select_button" id="selectA">A</button>
+      <button class="Select_button" id="selectB">B</button>
+      <button class="Select_button" id="selectC">C</button>
+      <button class="Select_button" id="selectD">D</button>
+      <button class="TextMessage_button" id="nextButton">Next</button>
+    `;
 
-    //Init the typewriter effect
+    // Initialize the typewriter effect
     this.revealingText = new RevealingText({
       element: this.element.querySelector(".TextMessage_p"),
       text: this.text
-    })
+    });
 
-    this.element.querySelector("button").addEventListener("click", () => {
-      //Close the text message
+    this.element.querySelector(".Select_button").addEventListener("click", () => {
+      // Close the text message
+      this.done();
+    });
+
+    this.element.querySelector("#nextButton").addEventListener("click", () => {
+      // Close the text message
       this.done();
     });
 
     this.actionListener = new KeyPressListener("Enter", () => {
       this.done();
-    })
-
+    });
   }
 
   done() {
-
     if (this.revealingText.isDone) {
       this.element.remove();
       this.actionListener.unbind();
@@ -45,8 +80,8 @@ class TextMessage {
 
   init(container) {
     this.createElement();
+    this.getQuestion();
     container.appendChild(this.element);
     this.revealingText.init();
   }
-
 }
